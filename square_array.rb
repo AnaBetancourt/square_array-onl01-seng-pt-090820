afa55cf2865b1264e22c6442684e2daa84200ad5
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 23:44:20 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -7,3 +7,10 @@ def square_array(array)
 end
 
 
+def square_array(array)
+  numbers = []
+  numbers.each do |number| #This is iterating over the empty array, not the `array` given as an argument
+    more_numbers << number # This should take individual elements from `array`, square them, then add them to the empty array, "numbers"
+  end
+  more_numbers # return the no-longer-empty numbers array
+end
