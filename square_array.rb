dbb2f565332af249a39b6cce224182f412f7e5c6
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-09-14 23:48:55 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,16 +1,7 @@
 def square_array(numbers)
   new_array = []
   numbers.each do |number|
-    numbers << number ** 2
+    new_array << number ** 2
   end
-  more_numbers
-end
-
-def square_array(numbers)
-  new_array = []
-  array.each do |number|
-    
-     number ** 2  #add squared number to new_array
-  end
-  #new_array
+  new_array
 end
